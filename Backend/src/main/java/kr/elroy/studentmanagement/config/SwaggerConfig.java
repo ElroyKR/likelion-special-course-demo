faad27edfbd31bc30a8ef79996e6062f1936a5ab
ElroyKR <elroykr@gmail.com>
2025-07-02 20:29:45 +0900

    chore: Swagger 설정

---
+++ Backend/src/main/java/kr/elroy/studentmanagement/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package kr.elroy.studentmanagement.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("학생 관리 시스템 API Spec")
+                        .version("v1")
+                );
+    }
+}
