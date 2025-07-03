9833e07ab64047b61b1472291fe320b00dc38b0f
ElroyKR <elroykr@gmail.com>
2025-07-03 17:11:46 +0900

    feat: add not found error codes for Department and Course domains

--- Backend/src/main/java/kr/elroy/studentmanagement/exception/ErrorCode.java
+++ Backend/src/main/java/kr/elroy/studentmanagement/exception/ErrorCode.java
@@ -7,8 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND("U0001", HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다.");
-
+    USER_NOT_FOUND("U0001", HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
+    DEPARTMENT_NOT_FOUND("D0001", HttpStatus.NOT_FOUND, "학과를 찾을 수 없습니다."),
+    COURSE_NOT_FOUND("C0001", HttpStatus.NOT_FOUND, "강의를 찾을 수 없습니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
