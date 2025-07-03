fd509cd181a81fb0e22da7274fc2b3eb93489d78
ElroyKR <elroykr@gmail.com>
2025-07-03 17:10:12 +0900

    fix: Enable jpa auditing

--- Backend/src/main/java/kr/elroy/studentmanagement/StudentManagementApplication.java
+++ Backend/src/main/java/kr/elroy/studentmanagement/StudentManagementApplication.java
@@ -2,12 +2,14 @@ package kr.elroy.studentmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StudentManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementApplication.class, args);
+    }
 
 }
